11b1287895071e7278626ce9a7cd5897eef805e9
Ridge Agyei <ibncastro20@gmail.com>
2020-09-02 21:13:45 +0000

    small loader fix

--- src/client/views/Register/AccountDetails.js
+++ src/client/views/Register/AccountDetails.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Error from '../../components/error'
 
 export class AccountDetails extends Component {
-  
   continue = (e) => {
     e.preventDefault();
     this.props.handleSubmit();
@@ -13,13 +13,18 @@ export class AccountDetails extends Component {
   };
 
   render() {
-    const { values, handleChange } = this.props;
+    const { values, handleChange, error, loading } = this.props;
     return (
       <div className="col-lg-6 col-12 p-0">
         <div className="card rounded-0 mb-0 p-2">
           <div className="card-header pt-50 pb-1">
             <div className="card-title">
               <h4 className="mb-0">Account Details</h4>
+              {error && (
+                <Error>
+                  <h4>There was an error loggin in!</h4>
+                </Error>
+              )}
             </div>
           </div>
           <div className="card-content">
@@ -83,7 +88,15 @@ export class AccountDetails extends Component {
                   type="submit"
                   className="btn btn-primary float-right btn-inline"
                 >
-                  Complete
+                  {loading ? (
+                    <span
+                      class="spinner-grow spinner-grow-sm"
+                      role="status"
+                      aria-hidden="true"
+                    ></span>
+                  ) : 
+                    "Complete"
+                  }
                 </button>
               </form>
             </div>
@@ -94,4 +107,4 @@ export class AccountDetails extends Component {
   }
 }
 
-export default AccountDetails
+export default AccountDetails;
--- src/client/views/loginF.js
+++ src/client/views/loginF.js
@@ -1,144 +1,159 @@
-import React, { Component } from 'react'
-import LoginMutation from '../components/mutations/login'
-import { Link } from 'react-router-dom'
-import Registration from './Register'
+import React, { Component } from "react";
+import LoginMutation from "../components/mutations/login";
+import Error from "../components/error";
+import Registration from "./Register";
 
-import loginImg from "../app-assets/images/pages/login.png"
+import loginImg from "../app-assets/images/pages/login.png";
 
- class Login extends Component {
-   state = {
-     showlogin: true,
-     email: "",
-     password: "",
-   };
+class Login extends Component {
+  state = {
+    showlogin: true,
+    email: "",
+    password: "",
+  };
 
-   login = (event) => {
-     event.preventDefault();
-     this.props.login({
-       variables: {
-         email: this.state.email,
-         password: this.state.password,
-       },
-     });
-   };
+  login = (event) => {
+    event.preventDefault();
+    this.props.login({
+      variables: {
+        email: this.state.email,
+        password: this.state.password,
+      },
+    });
+  };
 
-   render() {
+  render() {
     const { changeLoginState } = this.props;
-     const { error, loading } = this.props;
-     return (
-       <div className="app-content content">
-         <div className="content-overlay" />
-         <div className="header-navbar-shadow" />
-         <div className="content-wrapper">
-           <div className="content-header row"></div>
-           <div className="content-body"></div>
-           <section className="row flexbox-container">
-             <div className="col-xl-8 col-11 d-flex justify-content-center">
-               <div className="card bg-authentication rounded-0 mb-0">
-                 <div className="row m-0">
-                   <div className="col-lg-6 d-lg-block d-none text-center align-self-center px-1 py-0">
-                     <img src={loginImg} alt="branding logo" />
-                   </div>
-                   { this.state.showlogin && (
-                   <div className="col-lg-6 col-12 p-0">
-                     <div className="card rounded-0 mb-0 px-2">
-                       <div className="card-header pb-1">
-                         <div className="card-title">
-                           <h4 className="mb-0">Login</h4>
-                         </div>
-                       </div>
-                       <p className="px-2">
-                         Welcome back, please login to your account.
-                       </p>
-                       <div className="card-content pb-3">
-                         <div className="card-body pt-1">
-                           <form onSubmit={this.login}>
-                             <fieldset className="form-label-group form-group position-relative has-icon-left">
-                               <input
-                                 type="text"
-                                 onChange={(event) =>
-                                   this.setState({ email: event.target.value })
-                                 }
-                                 className="form-control"
-                                 id="user-name"
-                                 placeholder="Email"
-                                 required
-                               />
-                               <div className="form-control-position">
-                                 <i className="feather icon-user" />
-                               </div>
-                               <label htmlFor="user-name">Email</label>
-                             </fieldset>
-                             <fieldset className="form-label-group position-relative has-icon-left">
-                               <input
-                                 type="password"
-                                 className="form-control"
-                                 id="user-password"
-                                 placeholder="Password"
-                                 onChange={(event) =>
-                                   this.setState({
-                                     password: event.target.value,
-                                   })
-                                 }
-                                 required
-                               />
-                               <div className="form-control-position">
-                                 <i className="feather icon-lock" />
-                               </div>
-                               <label htmlFor="user-password">Password</label>
-                             </fieldset>
-                             <div className="form-group d-flex justify-content-between align-items-center">
-                               <div className="text-left">
-                                 
-                               </div>
-                               <div className="text-right">
-                                 <a
-                                   href="auth-forgot-password.html"
-                                   className="card-link"
-                                 >
-                                   Forgot Password?
-                                 </a>
-                               </div>
-                             </div>
-                             <button 
-                               className="btn btn-outline-primary float-left btn-inline"
-                               onClick={() => this.setState({showlogin: false})}
-                             >
-                               Register
-                             </button>
-                             <button
-                               type="submit"
-                               className="btn btn-primary float-right btn-inline"
-                             >
-                               Login
-                             </button>
-                           </form>
-                         </div>
-                       </div>
-                       
-                     </div>
-                   </div>
-                   )}
-                   { !this.state.showlogin && (
-                     <Registration changeLoginState={changeLoginState} showlogin={this.state.showlogin} />
-                   )}
-                 </div>
-               </div>
-             </div>
-           </section>
-         </div>
-       </div>
-     );
-   }
- }
+    const { error, loading } = this.props;
+    return (
+      <div className="app-content content">
+        <div className="content-overlay" />
+        <div className="header-navbar-shadow" />
+        <div className="content-wrapper">
+          <div className="content-header row"></div>
+          <div className="content-body"></div>
+          <section className="row flexbox-container">
+            <div className="col-xl-8 col-11 d-flex justify-content-center">
+              <div className="card bg-authentication rounded-0 mb-0">
+                <div className="row m-0">
+                  <div className="col-lg-6 d-lg-block d-none text-center align-self-center px-1 py-0">
+                    <img src={loginImg} alt="branding logo" />
+                  </div>
+                  {this.state.showlogin && (
+                    <div className="col-lg-6 col-12 p-0">
+                      <div className="card rounded-0 mb-0 px-2">
+                        <div className="card-header pb-1">
+                          <div className="card-title">
+                            <h4 className="mb-0">Login</h4>
+                            {error && (
+                              <Error>
+                                <h4>There was an error loggin in!</h4>
+                              </Error>
+                            )}
+                          </div>
+                        </div>
+                        <p className="px-2">
+                          Welcome back, please login to your account.
+                        </p>
+                        <div className="card-content pb-3">
+                          <div className="card-body pt-1">
+                            <form onSubmit={this.login}>
+                              <fieldset className="form-label-group form-group position-relative has-icon-left">
+                                <input
+                                  type="text"
+                                  onChange={(event) =>
+                                    this.setState({ email: event.target.value })
+                                  }
+                                  className="form-control"
+                                  id="user-name"
+                                  placeholder="Email"
+                                  required
+                                />
+                                <div className="form-control-position">
+                                  <i className="feather icon-user" />
+                                </div>
+                                <label htmlFor="user-name">Email</label>
+                              </fieldset>
+                              <fieldset className="form-label-group position-relative has-icon-left">
+                                <input
+                                  type="password"
+                                  className="form-control"
+                                  id="user-password"
+                                  placeholder="Password"
+                                  onChange={(event) =>
+                                    this.setState({
+                                      password: event.target.value,
+                                    })
+                                  }
+                                  required
+                                />
+                                <div className="form-control-position">
+                                  <i className="feather icon-lock" />
+                                </div>
+                                <label htmlFor="user-password">Password</label>
+                              </fieldset>
+                              <div className="form-group d-flex justify-content-between align-items-center">
+                                <div className="text-left"></div>
+                                <div className="text-right">
+                                  <a
+                                    href="auth-forgot-password.html"
+                                    className="card-link"
+                                  >
+                                    Forgot Password?
+                                  </a>
+                                </div>
+                              </div>
+                              <button
+                                className="btn btn-outline-primary float-left btn-inline"
+                                onClick={() =>
+                                  this.setState({ showlogin: false })
+                                }
+                              >
+                                Register
+                              </button>
+                              <button
+                                type="submit"
+                                className="btn btn-primary float-right btn-inline"
+                              >
+                                { loading ? (
+                                  <span
+                                    className="spinner-grow spinner-grow-sm"
+                                    role="status"
+                                    aria-hidden="true"
+                                  ></span>
+                                ) : (
+                                  "Login"
+                                )}
+                              </button>
+                            </form>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                  {!this.state.showlogin && (
+                    <Registration
+                      changeLoginState={changeLoginState}
+                      showlogin={this.state.showlogin}
+                    />
+                  )}
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default class LoginRegister extends Component {
-  render(){
+  render() {
     const { changeLoginState } = this.props;
     return (
-      <LoginMutation changeLoginState={changeLoginState} >
+      <LoginMutation changeLoginState={changeLoginState}>
         <Login changeLoginState={changeLoginState} />
       </LoginMutation>
-    )
+    );
   }
 }
